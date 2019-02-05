fd44c967fb9aa9a1c9d36e8dc1b4774a791706d6
Konstantin <skdin85@gmail.com>
2019-02-05 13:40:27 +0200

    Changed to StatefulWidet

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -2,20 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Intervit PS'),
-        ),
-        body: Card(child: Column(children: <Widget>[
-          Image.asset('assets/food.jpg'),
-          Text('Repair of matrix granulators')
-        ],),),
-      ),
+          appBar: AppBar(
+            title: Text('Intervit PS'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add Matrix'),
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/food.jpg'),
+                    Text('Repair of matrix granulators')
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
