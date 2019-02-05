30540cd0f271806dd6984228419650156d28684c
Konstantin <skdin85@gmail.com>
2019-02-05 12:48:56 +0200

    Added images

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp())
-}
+void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Intervit PS'),
         ),
+        body: Card(child: Column(children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text('Repair of matrix granulators')
+        ],),),
       ),
     );
   }
