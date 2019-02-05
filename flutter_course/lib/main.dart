81bce66eb74f21cfd7529ce5c85917d9904d1637
Konstantin <skdin85@gmail.com>
2019-02-05 15:38:28 +0200

    All parts are composed

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-
+import './product_manager.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,25 +11,8 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: Text('Intervit PS'),
           ),
-          body: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  onPressed: () {},
-                  child: Text('Add Matrix'),
-                ),
-              ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text('Repair of matrix granulators')
-                  ],
-                ),
-              ),
-            ],
-          )),
+          body: ProductManager(),
+    ),
     );
   }
 }
